9ef094091c6634a289f50adc83b5e0c1af50c489
albina <zaochnica@bk.ru>
2023-02-03 11:38:42 +0300

    Тестирование кнопки корзины сайта магазина на ее наличие для разных языков пользователей.

--- conftest.py
+++ conftest.py
@@ -4,7 +4,6 @@ from selenium.webdriver.chrome.options import Options
 
 
 def pytest_addoption(parser):
-    # parser.addoption('--browser_name', action='store', default='chrome', help="Choose browser: chrome or firefox")
     parser.addoption('--language', action='store', default='es', help="Choose lang")
 
 
